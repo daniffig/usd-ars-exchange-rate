ada0326510a6bf869f8ffa8e14fdb9fb2b046a37
Lucio Flavio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2018-11-21 14:15:14 -0300

    changed values to environment variables

--- app.rb
+++ app.rb
@@ -13,11 +13,12 @@ get '/usd-ars-exchange-rate' do
 end
 
 get '/usd-ars-exchange-rate.json' do
-  doc = Nokogiri::HTML(open('http://dolarhoy.com/cotizacion-dolar'))
+  # http://dolarhoy.com/cotizacion-dolar
+  doc = Nokogiri::HTML(open(ENV['EXCHANGE_RATE_URL']))
 
-  last_update_text = "#{doc.at_css('.update').text} -0300"
-  usd_buy_rate = doc.at_css('.compra').text.strip[-5..-1].sub!(',', '.').to_f
-  usd_sell_rate = doc.at_css('.venta').text.strip[-5..-1].sub!(',', '.').to_f
+  last_update_text = "#{doc.at_css(ENV['EXCHANGE_RATE_UPDATE_CLASS']).text} -0300"
+  usd_buy_rate = doc.at_css(ENV['EXCHANGE_RATE_BUY_CLASS']).text.strip[-5..-1].sub!(',', '.').to_f
+  usd_sell_rate = doc.at_css(ENV['EXCHANGE_RATE_SELL_CLASS']).text.strip[-5..-1].sub!(',', '.').to_f
 
   last_update_datetime = DateTime.strptime(last_update_text, '%d/%m/%y %H:%M %z')
 
@@ -27,5 +28,5 @@ get '/usd-ars-exchange-rate.json' do
     last_update: last_update_datetime,
     usd_buy_rate: usd_buy_rate,
     usd_sell_rate: usd_sell_rate
-  }.to_json
+  }.to_json  
 end
